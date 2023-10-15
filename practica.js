60e8b2beb2dc66c4ffa866002add5dcdb2a7e696
Natalia <natalia.rueda.ft@gmail.com>
2023-10-15 01:56:13 -0300

    Practicando JS

---
+++ practica.js
@@ -0,0 +1,18 @@
+console.log("¡Hoola Mundo!")
+console.log(3+1)
+console.log("Natalia + Rueda")
+console.log(3+false)
+console.log(3+true)
+//true=1
+//false=0
+//operadores comparacion: >  <  == !=
+console.log(3>1)
+console.log(3<1)
+
+
+//Variable
+
+var usuarioNetflix = "Alejandro"
+console.log(usuarioNetflix)
+console.log(usuarioNetflix)
+console.log("Bienvenido: "  + usuarioNetflix + ", Gracias!!")
